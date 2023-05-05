704402d2c07c02d6dda697d3ae5d494c3d502903
than <user@users-MacBook-Pro.local>
2023-05-05 16:49:22 +0700

    100SNT

---
+++ Bai2_VongLap/src/hienThi100SNT.java
@@ -0,0 +1,16 @@
+public class hienThi100SNT {
+    public static void main(String[] args) {
+
+        for (int j = 2; j < 100 ; j++) {
+            boolean check = true;
+            for (int k = 2; k <= Math.sqrt(j) ; k++) {
+                if (j%k==0){
+                    check = false;
+                    break;
+                }
+            }if (check) {
+                System.out.println("SNT la: " + j);
+            }
+        }
+    }
+}
