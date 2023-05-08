bf69e30f7a197a5bc823b401e754138d4049ac70
than <user@users-MacBook-Pro.local>
2023-05-08 17:30:19 +0700

    GopMang

---
+++ Bai3_Mang/src/GopMang.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class GopMang {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr1 = new int[3];
+        int[] arr2 = new int[3];
+        int[] arr3 = new int[arr1.length + arr2.length];
+        System.out.println("Nhập phần tử cho mảng 1: ");
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.println("Phần tử thứ: " + (i+1));
+            arr1[i] = scanner.nextInt();
+        }
+        System.out.println("Nhập phần tử cho mảng 2: ");
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.println("Phần tử thứ: " + (i+1));
+            arr2[i] = scanner.nextInt();
+        }
+        System.out.println(Arrays.toString(arr1) + Arrays.toString(arr2));
+
+        for (int i = 0; i < arr1.length; i++) {
+            arr3[i] = arr1[i];
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            arr3[arr1.length + i] = arr2[i];
+        }
+        System.out.println("Mảng Sau khi gộp là: " + Arrays.toString(arr3));
+    }
+}
