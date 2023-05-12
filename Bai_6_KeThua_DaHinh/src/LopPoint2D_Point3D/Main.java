b8be3a989262acfccbb59a2397b286675acf00fe
than <user@users-MacBook-Pro.local>
2023-05-12 11:23:33 +0700

    BT

---
+++ Bai_6_KeThua_DaHinh/src/LopPoint2D_Point3D/Main.java
@@ -0,0 +1,12 @@
+package LopPoint2D_Point3D;
+
+public class Main {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D(3,5);
+        System.out.println(point2D);
+
+        Point3D point3D = new Point3D(3,6,6);
+        System.out.println(point3D);
+    }
+
+}
